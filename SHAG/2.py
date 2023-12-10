1f2e8cf61e357dd371fc12d21348c1fedc87ba4f
DINEXXL <105819287+DINEXXL@users.noreply.github.com>
2023-12-10 22:46:16 +0200

    Update README.md

--- SHAG/2.py
+++ SHAG/2.py
@@ -80,6 +80,61 @@ class Student:
         self.end_of_day()
         self.is_alive()
 
+class Pet:
+
+    def __init__(self, name):
+        self.name = name
+        self.hunger = 0
+        self.happiness = 10
+        self.alive = True
+
+    def eat(self):
+        print('Time to eat')
+        self.hunger -= 3
+        if self.hunger < 0:
+            self.hunger = 0
+        self.happiness += 1
+
+    def sleep(self):
+        print("Time to sleep")
+        self.happiness += 2
+
+    def play(self):
+        print('Time to play')
+        self.happiness += 3
+        self.hunger += 2
+
+    def is_alive(self):
+        if self.hunger > 10:
+            print('Oh no, your pet has starved..')
+            self.alive = False
+        elif self.happiness <= 0:
+            print('Your pet died of sadness..')
+            self.alive = False
+
+    def end_of_day(self):
+        print(f'Happiness - {self.happiness}')
+        print(f'Hunger - {self.hunger}')
+
+    def live(self, day):
+        print(f'Day {day} of {self.name} life')
+        rnd = random.randint(1, 3)
+        if rnd == 1:
+            self.eat()
+        elif rnd == 2:
+            self.sleep()
+        else:
+            self.play()
+
+        self.end_of_day()
+        self.is_alive()
+
+fluffy = Pet(name='Fluffy')
+for day in range(365):
+    if not fluffy.alive:
+        break
+    fluffy.live(day)
+    
 nick = Student(name='Nick')
 for day in range(365):
     if not nick.alive:
