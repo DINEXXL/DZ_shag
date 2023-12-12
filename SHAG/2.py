44da027b6f18d25b802013046301fcd22cf33ae1
John Doe <johndoe@example.com>
2023-12-12 19:28:42 +0200

    modified:   SHAG/2.py

--- SHAG/2.py
+++ SHAG/2.py
@@ -6,125 +6,60 @@ class Student:
         self.name = name
         self.progress = 0
         self.gladness = 0
-        self.money = 100
+        self.money = 0
         self.alive = True
 
     def study(self):
-        print('Час вчити щось нове')
+        print('time to lear smth new')
         self.progress += 0.12
         self.gladness += 5
-        self.money -= 10
+        self.money += 10
 
     def sleep(self):
-        print("Час спати")
+        print("Time to sleep")
         self.gladness += 3
+        self.money -= 1
 
     def chill(self):
-        print('Час розслабитися')
+        print('Time to relax')
         self.gladness += 4
         self.progress -= 0.1
-        self.money -= 20
+        self.money -= 1
 
     def work(self):
-        print('Час працювати')
-        self.money += 50
+        print('Time to work')
+        self.money += 5
 
     def is_alive(self):
         if self.progress < -0.5:
-            print('Ой..')
+            print('Opps..')
             self.alive = False
         elif self.gladness <= 0:
-            print('Депресія..')
-            self.alive = False
-        elif self.money <= 0:
-            print('Брак коштів..')
-            self.alive = False
-
-    def end_of_day(self):
-        print(f'Радість - {self.gladness}')
-        print(f'Прогрес - {round(self.progress, 2)}')
-        print(f'Гроші - {self.money}')
-
-    def live(self, day):
-        print(f'День {day} життя {self.name}')
-        if self.money <= 0:
-            self.work()
-        elif self.progress < 0:
-            self.study()
-        else:
-            rnd = random.randint(1, 3)
-            if rnd == 1:
-                self.study()
-            elif rnd == 2:
-                self.sleep()
-            else:
-                self.chill()
-
-        self.end_of_day()
-        self.is_alive()
-
-class Cat:
-
-    def __init__(self, name):
-        self.name = name
-        self.hunger = 0
-        self.sleepiness = 0
-        self.happiness = 10
-        self.alive = True
-
-    def eat(self):
-        print(f'{self.name} is eating')
-        self.hunger -= 5
-        self.happiness += 1
-
-    def sleep(self):
-        print(f'{self.name} is sleeping')
-        self.sleepiness -= 5
-        self.happiness += 1
-
-    def play(self):
-        print(f'{self.name} is playing')
-        self.hunger += 2
-        self.sleepiness += 2
-        self.happiness += 3
-
-    def is_alive(self):
-        if self.hunger > 10:
-            print('Starvation..')
-            self.alive = False
-        elif self.sleepiness > 10:
-            print('Exhaustion..')
-            self.alive = False
-        elif self.happiness < 0:
             print('Depression..')
             self.alive = False
 
     def end_of_day(self):
-        print(f'Happiness - {self.happiness}')
-        print(f'Hunger - {self.hunger}')
-        print(f'Sleepiness - {self.sleepiness}')
+        print(f'Gladness - {self.gladness}')
+        print(f'Progress - {round(self.progress, 2)}')
+        print(f'Money - {self.money}')
 
     def live(self, day):
-        print(f'Day {day} of {self.name}\'s life')
-        if self.hunger > 7:
-            self.eat()
-        elif self.sleepiness > 7:
+        print(f'Day {day} of {self.name} life')
+        rnd = random.randint(1, 3)
+        if rnd == 1:
+            self.study()
+        elif rnd == 2:
             self.sleep()
+        elif rnd == 3:
+            self.chill()
         else:
-            self.play()
+            self.work()
 
         self.end_of_day()
         self.is_alive()
 
 nick = Student(name='Nick')
-KotRuguk228 = Cat(name='KotRuguk228')
-
 for day in range(365):
     if not nick.alive:
         break
     nick.live(day)
-
-for day in range(365):
-    if not KotRuguk228.alive:
-        break
-    KotRuguk228.live(day)
